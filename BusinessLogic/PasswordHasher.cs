6eb8f98854d35f2c1bcc78b7c130312b4d2de937
Oliver3214 <alexander.oliverpes@gmail.com>
2025-04-29 14:31:25 -0600

    se agregaron usuario y passwordHasher

---
+++ BusinessLogic/PasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BusinessLogic
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string contraseña)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(contraseña);
+                byte[] hashBytes = sha256.ComputeHash(bytes);
+
+                StringBuilder builder = new StringBuilder();
+                foreach (var b in hashBytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
--- BusinessLogic/UserService.cs
+++ BusinessLogic/UserService.cs
@@ -1,12 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Entities;
+using System.Data;
 
 namespace BusinessLogic
 {
-    internal class UserService
+    public class UserService
     {
+        private string connectionString = "Server=TU_SERVIDOR;Database=CitasDentales;User Id=CDENTAL;Password=OLIVER4209;";
+
+        public bool RegistrarUsuario(string usuario, string contraseña)
+        {
+            string contraseñaHash = PasswordHasher.HashPassword(contraseña);
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Usuarios (Usuario, Contraseña) VALUES (@usuario, @contraseña)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@usuario", usuario);
+                        cmd.Parameters.AddWithValue("@contraseña", contraseñaHash);
+
+                        int result = cmd.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    throw new Exception("Error al registrar usuario: " + ex.Message);
+                }
+            }
+        }
+
+        public bool ValidarUsuario(string usuario, string contraseña)
+        {
+            string contraseñaHash = PasswordHasher.HashPassword(contraseña);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM Usuarios WHERE Usuario = @usuario AND Contraseña = @contraseña";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@usuario", usuario);
+                    cmd.Parameters.AddWithValue("@contraseña", contraseñaHash);
+
+                    int count = (int)cmd.ExecuteScalar();
+                    return count > 0;
+                }
+            }
+        }
     }
 }
